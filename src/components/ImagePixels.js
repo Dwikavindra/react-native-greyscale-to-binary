a59a6513f8826b4b87be80300ccedeca05ba71d9
IDRISSOU Marzouk <islamsunnaestlaveriter@gmail.com>
2022-10-06 09:37:40 +0200

    This is a new commit for what I originally planned to be amended

---
+++ src/components/ImagePixels.js
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const ImagePixels = () => {
+  return (
+    <View>
+      <Text>ImagePixels</Text>
+    </View>
+  );
+};
+
+export default ImagePixels;
